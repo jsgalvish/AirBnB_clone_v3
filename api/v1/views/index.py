9ca933079dded066fb53e89e4e31dc249fc854c1
JS Galvis H <jsgalvish@unal.edu.co>
2020-01-30 10:32:56 -0500

    Status of your API
    Co-authored-by: zelda096 <miguelangel697@gmail.com>
    Co-authored-by: JS Galvis H <jsgalvish@unal.edu.co>

---
+++ api/v1/views/index.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module for app_views /status route"""
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+
+
+@app_views.route('/status', methods=["GET"])
+def status():
+    """Return /status api route"""
+    return jsonify({"status": "OK"})
+
+
